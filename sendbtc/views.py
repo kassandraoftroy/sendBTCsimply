2ecc60b375a761cabbff7031e08097a047582b45
Ari Rodriguez <superarius@gmail.com>
2018-02-24 12:26:22 -0500

    typos

--- sendbtc/views.py
+++ sendbtc/views.py
@@ -24,7 +24,7 @@ def ajax_tx(request):
 	except:
 		bytes_ = "Transaction Generation Failed:\nVerify all input fields.\nMake sure sender has enough funds for transaction and fee."
 	data = {'bytes_':bytes_}
-	return JsonResponse(data)d
+	return JsonResponse(data)
 
 def ajax_broadcast(request):
 
